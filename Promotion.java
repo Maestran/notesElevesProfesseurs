c1f23c4b89b3127cc15a547a28fe49ecec56527b
François GILLIOEN <francois.gillioen@efrei.net>
2019-04-04 09:52:40 +0200

    Classe Promotion

--- Promotion.java
+++ Promotion.java
@@ -1,4 +1,47 @@
 package notesElevesProfesseurs;
 
+import java.util.ArrayList;
+
 public class Promotion {
+
+        
+    private String nom;
+    private ArrayList<Eleve> eleves;
+    public Promotion(    ArrayList<Eleve> eleves)
+    {
+        this.eleves = eleves;
+    }
+    
+    Eleve RechercherEleve(int identifiant)
+    {
+        for(Eleve e : eleves)
+        {
+           if(e.getId()==identifiant)
+            return e;
+        }
+        return null;
+    }
+    /**
+     * @return the nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * @param nom the nom to set
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * @return the eleves
+     */
+    public ArrayList<Eleve> getEleves() {
+        return eleves;
+    }
+    
+
+    
 }
